54093776d655b97835ed12e472f40a48d1d65895
brockbyrd <brockbyrdd@gmail.com>
2021-01-13 16:43:19 -0600

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class Keypad extends Component {
+
+    constructor(){
+        super()
+    }
+
+    handleKeyUp = (keyUp) => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return(
+            <input type="password" onKeyUp={this.handleKeyUp} />
+        )
+    }
+}
